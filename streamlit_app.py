a7ff4ec8e56f434223c3b5d815da56841818ad2e
BHUVANESHBABU111 <68942299+BHUVANESHBABU111@users.noreply.github.com>
2023-03-19 15:13:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,5 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.header('fruityvice favourit foof')
